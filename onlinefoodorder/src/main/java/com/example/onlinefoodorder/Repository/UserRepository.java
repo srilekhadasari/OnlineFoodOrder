18d689481a8fc1c29d0e598287d434ca413c845c
sudhakarkandikattu <sudhakargoutam@gmail.com>
2024-12-08 20:57:09 +0530

    Added code for UserLogin

--- onlinefoodorder/src/main/java/com/example/onlinefoodorder/Repository/UserRepository.java
+++ onlinefoodorder/src/main/java/com/example/onlinefoodorder/Repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.onlinefoodorder.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import com.example.onlinefoodorder.Entities.Users;
 @Repository
 public interface UserRepository extends JpaRepository<Users,Long>{
 
-
+Optional<Users> findByEmail(String email);
     
 }
---
+++ onlinefoodorder/src/main/java/com/example/onlinefoodorder/Utility/UserLogin.java
@@ -0,0 +1,26 @@
+package com.example.onlinefoodorder.Utility;
+
+public class UserLogin {
+
+    private String email;
+    private String password;
+    public UserLogin(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+    
+}
